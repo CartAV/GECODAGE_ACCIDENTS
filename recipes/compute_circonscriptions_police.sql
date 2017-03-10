cfa81ef816978e433d978b039e34c30894691649
grabenertr <grabenertr>
2017-03-10 15:14:15 +0100

    Saved recipe: GECODAGE_ACCIDENTS.compute_circonscriptions_police

--- recipes/compute_circonscriptions_police.sql
+++ recipes/compute_circonscriptions_police.sql
@@ -1,2 +1,5 @@
-SELECT *
-  FROM "liste_communes_police_postgis"
+SELECT nom_circo, st_union(the_geom) as the_geom
+    FROM "liste_communes_police_postgis",
+         "IGN_COMMUNE_FRANCE"
+    GROUP BY nom_circo
+    
