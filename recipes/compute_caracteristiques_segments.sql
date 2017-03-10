49fba8f640f2492a953da351a2deb72763bde8ac
grabenertr <grabenertr>
2017-03-10 13:42:17 +0100

    Saved recipe: GECODAGE_ACCIDENTS.compute_caracteristiques_segments

--- recipes/compute_caracteristiques_segments.sql
+++ recipes/compute_caracteristiques_segments.sql
@@ -3,7 +3,7 @@ SELECT
     nearest_route.num_route_or_id,
     distance
 FROM "caracateristiques_postgis", 
-    LATERAL (SELECT "ID_RTE500", st_distance(geom_acc, the_geom) as distance
+    LATERAL (SELECT num_route_or_id, st_distance(st_point(longitude, latitude), the_geom) as distance
      FROM "osm_routes_par_commune" as routes
      where routes.the_geom && st_point(longitude, latitude)
      ORDER BY distance
