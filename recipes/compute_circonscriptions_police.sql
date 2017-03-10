cbfcfe8f2901a349b9597e71fb0dd2478f1b0142
grabenertr <grabenertr>
2017-03-10 15:16:41 +0100

    Saved recipe: GECODAGE_ACCIDENTS.compute_circonscriptions_police

--- recipes/compute_circonscriptions_police.sql
+++ recipes/compute_circonscriptions_police.sql
@@ -1,6 +1,6 @@
 SELECT nom_circo, st_union(the_geom::geometry) as the_geom
     FROM "liste_communes_police_postgis"
     INNER JOIN ign_commune_france
-        ON insee_com = commune_insee_long
+        ON "INSEE_COM" = commune_insee_long
     GROUP BY nom_circo
     
