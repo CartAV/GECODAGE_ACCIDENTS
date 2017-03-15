3bdfdb33d91bec0f06df198488b51e6a6fb874bb
grabenertr <grabenertr>
2017-03-15 18:09:53 +0100

    Saved recipe: GECODAGE_ACCIDENTS.compute_caracteristiques_segments

--- recipes/compute_caracteristiques_segments.sql
+++ recipes/compute_caracteristiques_segments.sql
@@ -4,7 +4,7 @@ SELECT
     nearest_route.num_route_or_id,
     distance
 FROM "caracateristiques_postgis", 
-    LATERAL (SELECT "INSEE_COM", num_route_or_id, st_distance(st_point(longitude, latitude), the_geom) as distance
+    LEFT JOIN LATERAL (SELECT "INSEE_COM", num_route_or_id, st_distance(st_point(longitude, latitude), the_geom) as distance
      FROM "osm_routes_par_commune" as routes
      where routes.the_geom && st_point(longitude, latitude)
      ORDER BY distance
