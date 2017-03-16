847f26b504250dcec3031045ada2c1eaecdebffd
grabenertr <grabenertr>
2017-03-16 14:52:55 +0100

    Saved recipe: GECODAGE_ACCIDENTS.compute_caracteristiques_segments_1

--- recipes/compute_caracteristiques_segments_1.sql
+++ recipes/compute_caracteristiques_segments_1.sql
@@ -1,13 +1,14 @@
 SELECT
     "Num_Acc",
-    nearest_route."INSEE_COM",
-    nearest_route.num_route_or_id,
-    distance
+    "IRIS",
+    "INSEE_COM",
+    "NOM_IRIS",
+    "IRIS",
+    "TYP_IRIS"
 FROM "caracateristiques_postgis" as caracs,
 
-LATERAL (SELECT "INSEE_COM", num_route_or_id, st_distance(st_point(longitude, latitude), the_geom) as distance
-     FROM "osm_routes_par_commune"  as routes
-     where routes.the_geom && st_point(longitude, latitude)
-     ORDER BY distance
-    LIMIT 1) as nearest_route
+LATERAL (SELECT *
+     FROM contours_iris as iris
+     where st_within(iris.the_geom::geometry, caracs.the_geom::geometry)
+    LIMIT 1) as iris
 
