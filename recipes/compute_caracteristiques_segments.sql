6826e4c7735ea10a9f8e5624ced30915c3fbcda2
grabenertr <grabenertr>
2017-03-16 11:33:19 +0100

    Saved recipe: GECODAGE_ACCIDENTS.compute_caracteristiques_segments

--- recipes/compute_caracteristiques_segments.sql
+++ recipes/compute_caracteristiques_segments.sql
@@ -2,10 +2,7 @@ SELECT
     "Num_Acc",
     nearest_route."INSEE_COM",
     nearest_route.num_route_or_id,
-    distance,
-    "CODE_IRIS",
-    "NOM_IRIS",
-    "TYP_IRIS"
+    distance
 FROM "caracateristiques_postgis" as caracs
 
 LEFT JOIN LATERAL (SELECT "INSEE_COM", num_route_or_id, st_distance(st_point(longitude, latitude), the_geom) as distance
