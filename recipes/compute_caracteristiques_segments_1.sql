c9677b87e4df88cf8fbfd60bce90dc0bee24c667
grabenertr <grabenertr>
2017-03-20 18:47:53 +0100

    Saved recipe: GECODAGE_ACCIDENTS.compute_caracteristiques_segments_1

--- recipes/compute_caracteristiques_segments_1.sql
+++ recipes/compute_caracteristiques_segments_1.sql
@@ -1,6 +1,7 @@
 SELECT
     "Num_Acc",
     "IRIS",
+    "CODE_IRIS",
     "INSEE_COM",
     "NOM_IRIS",
     "TYP_IRIS"
@@ -9,5 +10,6 @@ FROM "caracateristiques_postgis" as caracs,
 LATERAL (SELECT *
      FROM contours_iris as iris
      where st_within( caracs.the_geom::geometry, iris.the_geom::geometry)
+         AND "TYP_IRIS" = 'H'
     LIMIT 1) as iris
 
