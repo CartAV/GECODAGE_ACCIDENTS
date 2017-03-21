c6d20aeac5ebeef625357e24568b65c88b898017
grabenertr <grabenertr>
2017-03-21 11:51:07 +0100

    Saved recipe: GECODAGE_ACCIDENTS.compute_caracteristiques_segments_1

--- recipes/compute_caracteristiques_segments_1.sql
+++ recipes/compute_caracteristiques_segments_1.sql
@@ -5,10 +5,10 @@ SELECT
     "INSEE_COM",
     "NOM_IRIS",
     "TYP_IRIS"
-FROM "CONTOURS_IRIS_POPULATION" as caracs,
+FROM "caracateristiques_postgis" as caracs,
 
 LATERAL (SELECT *
-     FROM contours_iris as iris
+     FROM contours_iris_population as iris
      where st_within( caracs.the_geom::geometry, iris.the_geom::geometry)
            AND "TYP_IRIS" <> 'Z' -- les communes Z sont celles trop petites pour être découpées en IRIS
            AND "ban_citycode" = "INSEE_COM"
