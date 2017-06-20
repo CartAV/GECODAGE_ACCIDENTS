70d6f643af2e7b458629b10422ec1ae4aa876d5f
grabenertr <grabenertr>
2017-06-20 11:10:32 +0200

    Saved recipe: GECODAGE_ACCIDENTS.compute_caracateristiques_postgis

--- recipes/compute_caracateristiques_postgis.sql
+++ recipes/compute_caracateristiques_postgis.sql
@@ -1,2 +1,13 @@
-SELECT *
-  FROM "caracteristiques_coordinates_selected"
+SELECT accidents.*,
+       nearest_route.num_route_or_id,
+       nearest_route.num_route_com_id,
+       distance,
+       nearest_route.geojson
+  FROM "caracteristiques_coordinates_selected" as accidents
+
+LEFT JOIN LATERAL (SELECT "INSEE_COM", num_route_or_id, num_route_com_id, st_distance(st_point(accidents.longitude, accidents.latitude), the_geom) as distance, geojson
+     FROM "osm_routes_par_commune_geojson"  as routes
+     where routes.the_geom && st_point(longitude, latitude)
+     ORDER BY distance
+    LIMIT 1) as nearest_route
+ON true
