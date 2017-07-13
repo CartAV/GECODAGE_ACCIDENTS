ea828cf31e97ba9893c357b5f21c627ada6a3019
chabouisfr <chabouisfr>
2017-07-13 15:16:48 +0200

    Saved recipe: GECODAGE_ACCIDENTS.compute_lieux_prep_RN_PR

--- recipes/compute_lieux_prep_RN_PR.sql
+++ recipes/compute_lieux_prep_RN_PR.sql
@@ -1,2 +1,11 @@
-SELECT *
-  FROM "lieux_prep_rn_with_city_code"
+SELECT DISTINCT ON (lc."Num_Acc") lc.*, icf."INSEE_COM" as icf_city_code, b.lat as pr_lat, b.lon as pr_lon, b.lat_next as pr_lat_next, b.lon_next as pr_lon_next, b."INSEE_COM" as pr_insee_com, b."NOM_COM" as pr_nom_com 
+FROM "public"."lieux_caracteristiques_routes_nationales" lc
+LEFT JOIN "public"."ign_commune_france" icf
+ON
+    lc."current_city_code" = icf."INSEE_COM"
+LEFT JOIN  "public"."bornes_routes_borne_suivante_2016" b
+ON
+    lc."voie_normalise" = b."nom_courant_route" 
+    AND lc.pr = b.pr 
+    AND ST_DWithin(icf.the_geom::geography, ST_SetSRID(ST_MakePoint(b.lon, b.lat),4326)::geography, 2000)
+order by lc."Num_Acc", ST_distance(icf.the_geom, ST_SetSRID(ST_MakePoint(b.lon, b.lat),4326))
