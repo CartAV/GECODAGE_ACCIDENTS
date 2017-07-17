9d3cbb577b39a30ee98348cde18e8a522a5e2ebf
chabouisfr <chabouisfr>
2017-07-17 16:33:44 +0200

    Saved recipe: GECODAGE_ACCIDENTS.compute_caracateristiques_postgis

--- recipes/compute_caracateristiques_postgis.sql
+++ recipes/compute_caracateristiques_postgis.sql
@@ -3,11 +3,16 @@ SELECT accidents.*,
        nearest_route.num_route_com_id,
        distance,
        nearest_route.geojson
-  FROM "caracteristiques_coordinates_selected" as accidents
+  FROM "caracteristiques_coordinates_selected" AS accidents
 
-LEFT JOIN LATERAL (SELECT "INSEE_COM", num_route_or_id, num_route_com_id, st_distance(st_point(accidents.longitude, accidents.latitude), the_geom) as distance, geojson
-     FROM "osm_routes_par_commune_geojson"  as routes
-     where st_dwithin(routes.the_geom, st_point(longitude, latitude), 150)
+LEFT JOIN LATERAL (SELECT "INSEE_COM", num_route_or_id, num_route_com_id, st_distance(st_point(accidents.longitude, accidents.latitude), the_geom) AS distance, geojson
+     FROM "osm_routes_par_commune_geojson"  AS routes
+     WHERE
+        st_dwithin(routes.the_geom, st_point(longitude, latitude), 150)
+        AND (
+            (accidents.catr = 'autoroute' AND routes.osm_cat_route = 'autoroute')
+            OR (accidents.catr = 'route nationale' AND routes.osm_cat_route = 'route principale')
+            OR (accidents.catr != 'autoroute' AND accidents.catr != 'route nationale')
      ORDER BY distance
-    LIMIT 1) as nearest_route
+    LIMIT 1) AS nearest_route
 ON true
