6ecf9fe29d99f0e34661656a4e6a41e33d94db32
chabouisfr <chabouisfr>
2017-07-17 16:35:41 +0200

    Saved recipe: GECODAGE_ACCIDENTS.compute_caracateristiques_postgis

--- recipes/compute_caracateristiques_postgis.sql
+++ recipes/compute_caracateristiques_postgis.sql
@@ -8,7 +8,7 @@ SELECT accidents.*,
 LEFT JOIN LATERAL (SELECT "INSEE_COM", num_route_or_id, num_route_com_id, st_distance(st_point(accidents.longitude, accidents.latitude), the_geom) AS distance, geojson
      FROM "osm_routes_par_commune_geojson"  AS routes
      WHERE
-        st_dwithin(routes.the_geom, st_point(longitude, latitude), 150)
+        st_dwithin(routes.the_geom, st_point(longitude, latitude), 500)
         AND (
             (accidents.catr = 'autoroute' AND routes.osm_cat_route = 'autoroute')
             OR (accidents.catr = 'route nationale' AND routes.osm_cat_route = 'route principale')
