052f0b5f59a533606d47b0bd7e530ce5e8f388c0
grabenertr <grabenertr>
2017-10-09 18:08:52 +0200

    Saved recipe: GECODAGE_ACCIDENTS.compute_caracteristiques_arrondissements

--- recipes/compute_caracteristiques_arrondissements.sql
+++ recipes/compute_caracteristiques_arrondissements.sql
@@ -1,4 +1,4 @@
 SELECT *, ign_commune_france."CODE_COM" as code_arrondissement
   FROM "caracateristiques_postgis"
   LEFT JOIN ign_commune_france
-  ON st_within(st_point(longitude, latitude)::geography, "the_geom")
+  ON st_within(st_point(longitude, latitude), "the_geom"::geometry)
