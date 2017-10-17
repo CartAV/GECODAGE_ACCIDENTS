fbe9d2c95c573f31d4ed2c523286a50f3e796e53
grabenertr <grabenertr>
2017-10-17 14:37:00 +0200

    Saved recipe: GECODAGE_ACCIDENTS.compute_caracateristiques_postgis

--- recipes/compute_caracateristiques_postgis.sql
+++ recipes/compute_caracateristiques_postgis.sql
@@ -8,7 +8,11 @@ LEFT JOIN LATERAL (SELECT num_route_or_id,
                           num_route_com_id,
                           st_distance(st_point(accidents.longitude, accidents.latitude), the_geom) AS distance,
                           geojson,
-                          similarity(accidents.adr, num_route_or_id)
+                          similarity(accidents.adr, num_route_or_id) as similarity,
+                          (CASE WHEN similarity(accidents.adr, num_route_or_id) > 0.9 THEN 0
+                                WHEN similarity(accidents.adr, num_route_or_id) > 0.7 THEN 100
+                               ELSE st_distance(st_point(accidents.longitude, accidents.latitude), the_geom)
+                           END) as score
      FROM "osm_routes_par_commune_geojson"  AS routes
      WHERE
         st_dwithin(routes.the_geom, st_point(longitude, latitude), 500)
@@ -17,6 +21,6 @@ LEFT JOIN LATERAL (SELECT num_route_or_id,
             OR (accidents.catr = 'route nationale' AND routes.cat_route_osm = 'route principale')
             OR (accidents.catr = 'Boulevard Périphérique' AND routes.cat_route_osm = 'route principale')
             OR (accidents.catr != 'autoroute' AND accidents.catr != 'route nationale' AND accidents.catr != 'Boulevard Périphérique'))
-     ORDER BY distance
+     ORDER BY score
     LIMIT 1) AS nearest_route
 ON true
