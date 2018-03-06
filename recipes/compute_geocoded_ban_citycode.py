0b520023f9b7ef2ce230783be97254b6a80a28a8
antoinefa <antoinefa>
2018-03-06 19:42:31 +0100

    Saved recipe: GECODAGE_ACCIDENTS.compute_geocoded_ban_citycode

--- recipes/compute_geocoded_ban_citycode.py
+++ recipes/compute_geocoded_ban_citycode.py
@@ -38,9 +38,9 @@ error_col = '{}{}'.format(output_prefix,error_prefix) if error_prefix else None
 # Process config
 lines_per_request = 10
 verbosechunksize = 5000
-threads = 4
+threads = 8
 timeout = 500
-maxtries = 3
+maxtries = 5
 limit = None
 
 
