2aa6ca08f1c29d1bc0d4b4692a2d4201a75f9e00
antoinefa <antoinefa>
2018-09-25 14:08:33 +0000

    Saved recipe: GECODAGE_ACCIDENTS.compute_geocoded_ban_citycode

--- recipes/compute_geocoded_ban_citycode.py
+++ recipes/compute_geocoded_ban_citycode.py
@@ -5,6 +5,7 @@ from dataiku.customrecipe import get_input_names_for_role
 from dataiku.customrecipe import get_output_names_for_role
 from dataiku.customrecipe import get_recipe_config
 import itertools
+import os
 import logging
 import pandas as pd
 import requests
@@ -12,13 +13,13 @@ import StringIO
 import sys, time, traceback
 
 # Proxy and server  config
-
-PROXY_OPEN_LAB = 'proxy-1:3128'
-PROXY_PRIVATE_LAB = 'localhost:3128'
+os.environ['http_proxy']=''
+# PROXY_OPEN_LAB = '192.168.3.1:3128'
+# PROXY_PRIVATE_LAB = 'localhost:3128'
 
 SERVER_OPEN_LAB = 'http://datalab-ban'
-SERVER_PRIVATE_LAB = 'http://adresse.datalab.mi'
-SERVER_GOUV_FR = 'http://api-adresse.data.gouv.fr'
+# SERVER_PRIVATE_LAB = 'http://adresse.datalab.mi'
+# SERVER_GOUV_FR = 'http://api-adresse.data.gouv.fr'
 
 OPEN_LAB = True
 
