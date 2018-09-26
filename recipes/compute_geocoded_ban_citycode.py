855f47a8968a0f8927aecf09361e0767fd6432ea
dupontlau <dupontlau>
2018-09-26 08:54:49 +0000

    Saved recipe: GECODAGE_ACCIDENTS.compute_geocoded_ban_citycode

--- recipes/compute_geocoded_ban_citycode.py
+++ recipes/compute_geocoded_ban_citycode.py
@@ -13,18 +13,9 @@ import StringIO
 import sys, time, traceback
 
 # Proxy and server  config
-os.environ['http_proxy']=''
-# PROXY_OPEN_LAB = '192.168.3.1:3128'
-# PROXY_PRIVATE_LAB = 'localhost:3128'
-
-SERVER_OPEN_LAB = 'http://datalab-ban'
-# SERVER_PRIVATE_LAB = 'http://adresse.datalab.mi'
-# SERVER_GOUV_FR = 'http://api-adresse.data.gouv.fr'
-
-OPEN_LAB = True
-
-http_proxy = None #PROXY_OPEN_LAB if OPEN_LAB else PROXY_PRIVATE_LAB
-server_address = SERVER_OPEN_LAB if OPEN_LAB else SERVER_PRIVATE_LAB
+os.environ['http_proxy'] = ''
+http_proxy = None
+server_address = 'http://datalab-ban'
 
 # Input fields configuration
 columns = ['adr']
