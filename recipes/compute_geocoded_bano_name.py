cd04a0d58a68811c96a8519033e3093a45da92bd
antoinefa <antoinefa>
2018-10-04 23:54:16 +0000

    Saved recipe: GECODAGE_ACCIDENTS.compute_geocoded_bano_name

--- recipes/compute_geocoded_bano_name.py
+++ recipes/compute_geocoded_bano_name.py
@@ -77,7 +77,6 @@ def adresse_submit(df,i=0,schema_check=[]):
     df.reset_index(inplace=True)
     for col in cols:
         df[col].replace(np.nan,"xxxxx",regex=True) 
-        df[col].replace(r'^\s*$',"xxxxx",regex=True)    
     df[cols].to_csv(string_io, encoding="utf-8", index=False)
     kwargs = {
         'data': data,
