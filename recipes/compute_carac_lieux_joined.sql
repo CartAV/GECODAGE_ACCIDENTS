e47c23f7b8c1ec5817a046cb4f7b3ede0701bb2c
dupontlau <dupontlau>
2018-10-08 15:12:59 +0000

    Saved recipe: GECODAGE_ACCIDENTS.compute_carac_lieux_joined

--- recipes/compute_carac_lieux_joined.sql
+++ recipes/compute_carac_lieux_joined.sql
@@ -12,7 +12,7 @@ FROM(
     "caracteristiques_coordinates_selected_joined" AS accidents
      LEFT JOIN --LATERAL 
       "osm_routes_par_commune"  AS routes
-      WHERE st_dwithin(routes.the_geom, st_point(longitude, latitude), 500)
+       WHERE st_dwithin(routes.the_geom, st_point(longitude, latitude), 500)
     AND ((accidents.catr = 'autoroute' AND routes.cat_route_osm = 'autoroute')
         OR (accidents.catr = 'route nationale' AND routes.cat_route_osm = 'route principale')
         OR (accidents.catr = 'Boulevard Périphérique' AND routes.cat_route_osm = 'route principale')
