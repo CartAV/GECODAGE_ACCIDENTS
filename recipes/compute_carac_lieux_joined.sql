2589bf03c373beaba75d284c2bf99588d5ab442a
dupontlau <dupontlau>
2018-10-08 15:17:06 +0000

    Saved recipe: GECODAGE_ACCIDENTS.compute_carac_lieux_joined

--- recipes/compute_carac_lieux_joined.sql
+++ recipes/compute_carac_lieux_joined.sql
@@ -9,10 +9,7 @@ SELECT accidents.*,
          ELSE st_distance(st_point(accidents.longitude, accidents.latitude), route.the_geom) * (1 - similarity(accidents.adr, route.num_route_or_id))
          END) AS score
 FROM
-    "caracteristiques_coordinates_selected_joined" AS accidents
-     LEFT JOIN LATERAL 
-     "osm_routes_par_commune"  AS routes
-    ON true
+    "caracteristiques_coordinates_selected_joined" AS accidents LEFT JOIN LATERAL "osm_routes_par_commune"  AS routes
      WHERE st_dwithin(routes.the_geom, st_point(longitude, latitude), 500)
      AND ((accidents.catr = 'autoroute' AND routes.cat_route_osm = 'autoroute')
         OR (accidents.catr = 'route nationale' AND routes.cat_route_osm = 'route principale')
@@ -21,3 +18,4 @@ FROM
     )
     ORDER BY score, distance
     LIMIT 1
+    ON true
