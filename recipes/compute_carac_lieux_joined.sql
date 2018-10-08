6cba6177959ccf2ba709c3b582320c7b6123c56b
dupontlau <dupontlau>
2018-10-08 15:15:13 +0000

    Saved recipe: GECODAGE_ACCIDENTS.compute_carac_lieux_joined

--- recipes/compute_carac_lieux_joined.sql
+++ recipes/compute_carac_lieux_joined.sql
@@ -1,7 +1,5 @@
 SELECT * FROM
     "caracteristiques_coordinates_selected_joined" AS accidents
      LEFT JOIN --LATERAL 
-      "osm_routes_par_commune"  AS routes
-    ORDER BY score, distance
-    LIMIT 1
+      "osm_routes_par_commune"  AS routes 
     ON true
