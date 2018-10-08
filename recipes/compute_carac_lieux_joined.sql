6f61e572be955f006f2a9c958bb37d111d4a0c5a
dupontlau <dupontlau>
2018-10-08 15:08:12 +0000

    Saved recipe: GECODAGE_ACCIDENTS.compute_carac_lieux_joined

--- recipes/compute_carac_lieux_joined.sql
+++ recipes/compute_carac_lieux_joined.sql
@@ -3,11 +3,11 @@ SELECT accidents.*,
        route.num_route_com_id,
        route.st_distance(st_point(accidents.longitude, accidents.latitude), the_geom) AS distance,
        route.geojson,
-       route.similarity(accidents.adr, num_route_or_id) as similarity,
+       route.similarity(accidents.adr, num_route_or_id) AS similarity,
        (CASE WHEN similarity(accidents.adr, route.num_route_or_id) > 0.7
          THEN 0
          ELSE st_distance(st_point(accidents.longitude, accidents.latitude), route.the_geom) * (1 - similarity(accidents.adr, route.num_route_or_id))
-         END) as score
+         END) AS score
 FROM(
     "caracteristiques_coordinates_selected_joined" AS accidents
     LEFT JOIN LATERAL "osm_routes_par_commune"  AS routes
@@ -19,3 +19,4 @@ FROM(
     )
     ORDER BY score, distance
     LIMIT 1
+)
